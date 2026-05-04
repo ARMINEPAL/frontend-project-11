b28b45f7ba1e6d3991c3b093a5a59588ff55bdde
Armine Palanjyan <a.p.t2@mail.ru>
2026-05-04 13:18:42 +0400

    RSS loading

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const defaultLanguage = 'ru'
 setLocale({
   mixed: {
     required: () => ({ key: 'errors.required'}),
+    notOneOf: () => ({ key: 'errors.shoudEqual'})
   },
   string: {
     url: () => ({ key: 'errors.url'}),
