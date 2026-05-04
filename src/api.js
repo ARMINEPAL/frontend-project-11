893bd0a1d646cec3aaf008cebb7df8ac3ee7741d
Armine Palanjyan <a.p.t2@mail.ru>
2026-05-04 16:37:21 +0400

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const getProxyUrl = (url) => {
-    return `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&cache=false`
+    const proxy = 'https://allorigins.hexlet.app/get'
+    return `${proxy}?disableCache=true&url=${encodeURIComponent(url)}`
   }
   
 const load = async (url) => {
